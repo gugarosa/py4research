91bec3bdd271ca47bbcf48b58e1cf077267f2b40
Gustavo Rosa <gth.rosa@uol.com.br>
2021-11-25 18:46:09 -0300

    chore(py4research): Adds setup and requirements file.

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='py4research',
+      version='1.0.0',
+      description='Build a Python package for research',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Gustavo Rosa',
+      author_email='gustavo.rosa@unesp.br',
+      url='https://github.com/gugarosa/py4research',
+      license='MIT',
+      install_requires=['coverage>=5.5',
+                        'numpy>=1.19.5',
+                        'pylint>=2.7.2',
+                        'pytest>=6.2.2'
+                        ],
+      extras_require={
+          'tests': ['coverage',
+                    'pytest',
+                    'pytest-pep8',
+                    ],
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
